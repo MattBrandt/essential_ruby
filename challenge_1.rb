93ac2bff86de6d29d1fdedcd21b1782303e5a52b
matt brandt <mattbrandt87@gmail.com>
2014-04-28 17:31:42 -0500

    Challenge 1

--- challenge_1.rb
+++ challenge_1.rb
@@ -14,10 +14,30 @@
 #   http://en.wikipedia.org/wiki/Mortgage_calculator
 
 def pmt(rate, nper, pv)
+  rate = interest_rate.to_f
+  nper = number_of_payments.to_i
+  pv = principal_value.to_i
   # ============================================
   # Your code to implement the method goes here.
   # ============================================
 end
+
+puts 'What is your rate?'
+puts 'Please put your rate in the form of a decimal i.e.'
+puts '2.5 percent would be typed in as .025'
+rate = gets.chomp
+
+
+puts 'How many monthly payments do you plan on making'
+nper = gets.chomp
+
+puts 'what is the principal of the loan'
+puts 'please round to the nearest dollar and do not use symbols'
+pv = gets.chomp
+
+
+
+
 
 # Example usage of the method:
 
